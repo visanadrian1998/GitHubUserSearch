bb929633554962f751950755ad80f5c9e2366b13
adrian_visan1998 <visanadrian1998@gmail.com>
2020-11-13 19:59:00 +0200

    work in progress

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
-import {NavigationContainer,DefaultTheme,DarkTheme} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Screen1 from "./src/screens/Screen1";
 import Screen2 from "./src/screens/Screen2";
-import { StyleSheet, Text, View, Button } from 'react-native';
-import ToggleSwitch from 'toggle-switch-react-native'
+import { StyleSheet, Text, View, Button } from "react-native";
+import ToggleSwitch from "toggle-switch-react-native";
 
-const {Navigator, Screen} = createStackNavigator();
-
+const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
-
-  const [boolean,setBoolean] = useState(false);
+  const [boolean, setBoolean] = useState(false);
 
   return (
     <NavigationContainer theme={boolean === true ? DarkTheme : DefaultTheme}>
-      <Navigator initialRouteName="Screen1" screenOptions={{
+      <Navigator
+        initialRouteName="Screen1"
+        screenOptions={{
           headerRight: () => (
-  <ToggleSwitch
-  isOn={boolean}
-  onColor="white"
-  offColor="black"
-  labelStyle={{ color: "black", fontWeight: "900" }}
-  size="medium"
-  onToggle={() => setBoolean(!boolean)}
-/>
-)
-      }}>
-        <Screen name="Screen1" component={Screen1} options={{
-          
-        }} />
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                paddingRight: 10,
+              }}
+            >
+              <ToggleSwitch
+                isOn={boolean}
+                onColor="white"
+                offColor="black"
+                labelStyle={{ color: "black", fontWeight: "900" }}
+                size="medium"
+                onToggle={() => setBoolean(!boolean)}
+              />
+            </View>
+          ),
+        }}
+      >
+        <Screen name="Screen1" component={Screen1} options={{}} />
         <Screen name="Screen2" component={Screen2} />
       </Navigator>
     </NavigationContainer>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/Screen2.js
+++ src/screens/Screen2.js
@@ -1,13 +1,76 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const Screen2 = ({route}) => {
+const Screen2 = ({ route }) => {
   const { array } = route.params;
+  array.sort(function (a, b) {
+    return parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count);
+  });
+  array.length > 10 ? (array.length = 10) : "";
   return (
-    <View>
-    <Text>{JSON.stringify(array)}</Text>
-    </View>
+    <SafeAreaView>
+      {array.length == 0 ? (
+        <Text style={styles.noReposText}>
+          Unfortunately, this user has 0 repositories.
+        </Text>
+      ) : (
+        <>
+          <View style={styles.listHeader}>
+            <Text style={styles.listName}>Name</Text>
+            <Text style={styles.listName}>Number of stars</Text>
+          </View>
+          <FlatList
+            contentContainerStyle={{ paddingBottom: 45 }}
+            data={array}
+            keyExtractor={(item) => item.node_id}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={styles.listItem}>
+                <Text style={styles.listName}>{item.name}</Text>
+                <Text style={styles.listName}>{item.stargazers_count}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </>
+      )}
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  listItem: {
+    backgroundColor: "#009688",
+    flexDirection: "row",
+    marginTop: 10,
+    marginHorizontal: 15,
+    padding: 10,
+    borderRadius: 15,
+  },
+  listHeader: {
+    backgroundColor: "#009688",
+    flexDirection: "row",
+    padding: 10,
+  },
+  listName: {
+    flex: 0.5,
+    textAlign: "center",
+    alignItems: "flex-start",
+  },
+  listStars: {
+    flex: 0.5,
+    textAlign: "center",
+    alignItems: "flex-end",
+  },
+  noReposText: {
+    color: "#009688",
+    paddingLeft: 10,
+  },
+});
 
 export default Screen2;
